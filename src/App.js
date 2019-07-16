9f6b756ba5abdf798d42a0c56f1d93f74be3dbd5
Patryk Stanek <p.stanek3@gmail.com>
2019-07-16 17:15:47 +0200

    added basic styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import PlayersList from './components/PlayersList/PlayersList';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor() {
+    super();
 
-    </div>
-  );
+    this.state = {
+      players: [
+        {
+            name: 'Kasia',
+            score: 5
+        },
+        {
+            name: 'Basia',
+            score: 0
+        }
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <PlayersList players={this.state.players} />
+      </div>
+    );
+  }
 }
 
 export default App;
